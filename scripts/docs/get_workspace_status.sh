30828a5dde967f3cc88c6bb16285b5e5afa420d6
fweikert <fwe@google.com>
2022-04-08 19:46:04 +0200

    WIP BAK: branch name

--- scripts/docs/get_workspace_status.sh
+++ scripts/docs/get_workspace_status.sh
@@ -1,10 +1,19 @@
-if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
-  git_rev=$(git rev-parse --abbrev-ref HEAD)
-  if [[ $? != 0 ]];
-  then
-      exit 1
-  fi
-  echo "BUILD_SCM_REVISION ${git_rev}"
+#if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
+#  git_rev=$(git rev-parse --abbrev-ref HEAD)
+#  if [[ $? != 0 ]];
+#  then
+#      exit 1
+#  fi
+#  echo "BUILD_SCM_REVISION ${git_rev}"
+#else
+#  echo "BUILD_SCM_REVISION no_git"
+#fi;
+
+RELEASE_NAME=$(source scripts/release/common.sh; get_full_release_name)
+
+if [[ -z "$RELEASE_NAME" ]]; then
+  echo "Failed to get a release name, are you on a release branch?"
+  exit 1
 else
-  echo "BUILD_SCM_REVISION no_git"
-fi;
+  echo "BUILD_SCM_REVISION $RELEASE_NAME"
+fi
